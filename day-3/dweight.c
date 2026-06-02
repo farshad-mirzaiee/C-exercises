5f42ca58164c8fa8905f5743d17f95b2fb3f82b4
Ali Farshad Mirzaiee <118207714+farshad-mirzaiee@users.noreply.github.com>
2026-06-02 14:51:29 +0200

    Computing dimensional weight of box using input from user.

--- day-3/dweight.c
+++ day-3/dweight.c
@@ -4,5 +4,19 @@
 
 int main(void)
 {
-    
+    int height, length, width, volume, weight;
+
+    printf("Enter the height of the box: ");
+    scanf("%d", &height);
+    printf("Enter the length of the box: ");
+    scanf("%d", &length);
+    printf("Enter the width of the box: ");
+    scanf("%d", &width);
+    volume = height * length * width; 
+    weight = (volume + 165) / 166;
+
+    printf("Volume (cubic inches) : %d\n", volume);
+    printf("Dimensional Weight (pounds) : %d\n", weight);
+
+    return 0;
 }
