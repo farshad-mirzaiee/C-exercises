cca7728d4d96af9ca4484a9b22cd7a149392d4ae
Ali Farshad Mirzaiee <118207714+farshad-mirzaiee@users.noreply.github.com>
2026-07-05 10:44:44 +0200

    Revised the program to get input from user.

--- day-5/dweight.c
+++ day-5/dweight.c
@@ -4,13 +4,16 @@ int main(void)
 {
     int height, length, width, volume, weight;
 
-    height = 8;
-    length = 12;
-    width = 10;
+    printf("Enter the height of the box: ");
+    scanf("%d", &height);
+    printf("Enter the length of the box: ");
+    scanf("%d", &length);
+    printf("Enter the width of the box: ");
+    scanf("%d", &width);
     volume = height * length * width;
     weight = (volume + 165) / 166;
 
-    printf("Dimensions: %dx%dx%d\n", height, length, width);
+    
     printf("Volume (cubic inches): %d\n", volume);
     printf("Dimensional weight (pounds): %d\n", weight);
 
