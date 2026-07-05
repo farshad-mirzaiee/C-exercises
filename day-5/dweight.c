8ff271c80298ce6bd5f44f117a996156dc47911e
Ali Farshad Mirzaiee <118207714+farshad-mirzaiee@users.noreply.github.com>
2026-07-05 10:53:34 +0200

    Revised the program

--- day-5/dweight.c
+++ day-5/dweight.c
@@ -4,18 +4,16 @@ int main(void)
 {
     int height, length, width, volume, weight;
 
-    printf("Enter the height of the box: ");
+    printf("Enter the height: ");
     scanf("%d", &height);
-    printf("Enter the length of the box: ");
+    printf("Enter the length: ");
     scanf("%d", &length);
-    printf("Enter the width of the box: ");
+    printf("Enter the width: " );
     scanf("%d", &width);
-    volume = height * length * width;
+
+    volume = height * length * width; 
     weight = (volume + 165) / 166;
 
-    
     printf("Volume (cubic inches): %d\n", volume);
     printf("Dimensional weight (pounds): %d\n", weight);
-
-    return 0;
 }
