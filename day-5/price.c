3992d79ca2a7fdba7f8874fcaf4a2ed72269ffc3
Ali Farshad Mirzaiee <118207714+farshad-mirzaiee@users.noreply.github.com>
2026-07-05 18:12:17 +0200

    Prompts user for an amound and displays the total with tax added.

---
+++ day-5/price.c
@@ -0,0 +1,24 @@
+/*
+ * Prompts the user for an amount and displays the total with tax added.
+ * C programming language exercise. 
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+    const float TAX_RATE = 5.0f;
+
+    float amount, total, tax;
+
+    printf("Enter an amount: ");
+    scanf("%f", &amount);
+
+    tax = amount * TAX_RATE / 100.0f;
+    printf("The tax is: %.2f\n", tax);
+
+    total = amount + tax;
+
+    printf("With tax added: %.2f\n", total);
+    return 0;
+}
