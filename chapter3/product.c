50baedb0f26a8267a5715bd947fc598924d786bf
Ali Farshad Mirzaiee <118207714+farshad-mirzaiee@users.noreply.github.com>
2026-08-02 11:50:14 +0200

    C programming: Ch3. Project 2

---
+++ chapter3/product.c
@@ -0,0 +1,27 @@
+/*
+ * Prompts the user to enter product information and displays it in tabular form.
+ * K.N King's C Programming: A Modern Approach
+ * Program Projects - Chapter 3 - Project 2
+ * By Farshad Mirzaiee
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+
+    int item, day, month, year;
+    float price;
+
+    printf("Enter item number: ");
+    scanf("%d", &item);
+    printf("Enter Unit Price: ");
+    scanf("%f", &price);
+    printf("Enter Purchase Date (mm/dd/year): " );
+    scanf("%d/%d/%d", &month, &day, &year);
+
+    printf("Item\tUnit\tPurchase\n\tprice\tdate\n");
+    printf("%-5d\t$%7.2f\t%.2d/%.2d/%d\n", item, price, month, day, year);
+
+    return 0;
+}
