f669c1090dc714ec2d3797db2e3a39ce7d1c2692
Ali Farshad Mirzaiee <118207714+farshad-mirzaiee@users.noreply.github.com>
2026-08-02 11:51:41 +0200

    Modified the program.

--- chapter3/product.c
+++ chapter3/product.c
@@ -15,7 +15,7 @@ int main(void)
 
     printf("Enter item number: ");
     scanf("%d", &item);
-    printf("Enter Unit Price: ");
+    printf("Enter unit price: ");
     scanf("%f", &price);
     printf("Enter Purchase Date (mm/dd/year): " );
     scanf("%d/%d/%d", &month, &day, &year);
